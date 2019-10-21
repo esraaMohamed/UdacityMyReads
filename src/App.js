e157b13d8c0842889ffe700e861c097e9889486f
esraa <esraa.mohamed87@gmail.com>
2019-10-21 09:44:13 -0700

    Adding BrowserRouter in app class

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MainPage from './components/MainPage'
+import {BrowserRouter} from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
-
-  render() {
-    return (
-      <div className="app">
-       <MainPage />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <BrowserRouter>
+              <MainPage/>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default BooksApp
